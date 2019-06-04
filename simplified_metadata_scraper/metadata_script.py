64923745f356b3cc8ea07fbe5c1eeaa603ec0781
Anand <anandc823@gmail.com>
2019-06-04 13:08:03 -0400

    added fixes for missing minutes

--- simplified_metadata_scraper/metadata_script.py
+++ simplified_metadata_scraper/metadata_script.py
@@ -1,35 +1,54 @@
-#Author: Anand Chitale June 3rd 2013
+#Author: Anand Chitale 2019
 import requests
 from bs4 import BeautifulSoup
+from bs4 import Tag
 import csv
 
 def main():
     documents = []
-    start_date = 1936
-    end_date = 2013
-
+    start_date, end_date = 1936,2013
+    uid = 1
     year_pages = get_year_pages(start_date,end_date)
     for year in year_pages:
         soup = BeautifulSoup(year,'lxml')
+
         #Article Div Tag Contains All Meeting Tables
-        meeting_container = soup.find("div",id="article")
+        meeting_container = soup.find("div", id="article")
         year = meeting_container.find("h3").text
+
         #contains a table of all documents in a meeting
         meeting_tables = meeting_container.findChildren("div",recursive=False)
+
         for meeting_table in meeting_tables:
             #header which contains date and type of event
             meeting_info = meeting_table.find("h5").text
+
+            meeting_document_tables = meeting_table.find_all("div")[1]
+
             #each p tag contains a set of one of more links to documents of a type
-            materials = meeting_table.find_all("p")
+            materials = meeting_document_tables.find_all("p")
             for material in materials:
-                documents_and_links = get_documents_and_links(material)
+                document_list = get_documents_and_links(material)
                 #Append Meeting Specific Information to each document
-                for document in documents_and_links:
-                    document['meeting_info'] = meeting_info
-                    document['year'] = year
-                    documents.append(document)
+                for document in document_list:
+                    add_document(documents,document,year,meeting_info,uid)
+                    uid+=1
+
+            #Catches any minutes that are not given in a p-tag, just written as text in the div
+            meeting_document_lists = meeting_document_tables.find_all("div")
+            for meeting_document_list in meeting_document_lists:
+                non_p_text = ''.join(meeting_document_list.find_all(text=True, recursive=False))
+                non_p_text += get_non_link_inner_text(meeting_document_list)
+                if non_p_text and non_p_text.strip():
+                    non_p_text = ' '.join(non_p_text.split())
+                    document = {'document_name':non_p_text,
+                                      'link': None
+                                      }
+                    add_document(documents,document,year,meeting_info,uid)
+                    uid+=1
     #writes to csv file in current working directory
     write_to_csv(documents)
+
 def get_year_pages(start_date,end_date):
     base_time_url = "https://www.federalreserve.gov/monetarypolicy/fomchistorical"
 
@@ -49,33 +68,47 @@ def get_year_pages(start_date,end_date):
 
 def get_documents_and_links(document_type):
     documents = []
-    ptext = document_type.find(text=True,recursive=False)
-    doc_name = ''
+    document_name = ''
+    ptext = document_type.find(text=True, recursive=False)
     if ptext and ptext.strip():
-        doc_name = ptext
+        document_name = ptext
+    document_name += get_non_link_inner_text(document_type)
     links = document_type.find_all("a")
     if not links:
         current_document = {}
-        current_document['document_name'] = doc_name
+        current_document['document_name'] = document_name
         current_document['link'] = None
         documents.append(current_document)
     else:
         for link in links:
-            cur_doc_name = doc_name + link.text
+            cur_doc_name = document_name + link.text
             current_document = {}
             current_document['document_name'] = cur_doc_name
             current_document['link'] = link.get("href")
             documents.append(current_document)
     return documents
 
+def add_document(documents,document,year,meeting_info,uid):
+    document['year'] = year
+    document['meeting_info'] = meeting_info
+    document['id'] = uid
+    documents.append(document)
+
 # Writes information to CSV File
 def write_to_csv(documents):
     with open('test_metadata.csv', 'w') as csvfile:
-        fieldnames = ['year', 'meeting_info', 'document_name', 'link']
+        fieldnames = ['id', 'year', 'meeting_info', 'document_name', 'link']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for document in documents:
             writer.writerow(document)
+
+def get_non_link_inner_text(tag):
+    inner_text = ''
+    for child in tag.find_all(recursive=False):
+        if type(child) == Tag and child.name == "em":
+             inner_text += child.text
+    return inner_text
 
 if __name__ == "__main__":
     main()
