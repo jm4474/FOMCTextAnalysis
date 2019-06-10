aef7c6c6110b882dc92df9cc28d21e67b7b778f8
Anand <anandc823@gmail.com>
2019-06-10 16:07:00 -0400

    added bluebooks going back to 1965 and script

--- simplified_metadata_scraper/python/scripts/statement_download_script.py
+++ simplified_metadata_scraper/python/scripts/statement_download_script.py
@@ -3,10 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import csv
+import tika
+from tika import parser
 def main():
     data_file_name = "../output/derived_data.csv"
-    grouping = "Statement"
-    start_year = 1993
+    grouping = "Bluebook"
+    start_year = 1965
     end_year = 2013
     df = pd.read_csv(data_file_name)
     files = []
@@ -20,40 +22,59 @@ def main():
                           'year':row['year']
                           }
                          )
-    statements = []
+    documents = []
     for file in files:
-        statement = {
-            'start_date': file['start_date'],
-            'end_date': file['end_date']
-        }
-        file_text = ""
-        r = requests.get(file['link']).content
-        soup = BeautifulSoup(r,'lxml')
-        p_tags = soup.find_all("p",attrs={'class':None})
-        for p_tag in p_tags:
-            if p_tag.text and p_tag.text.strip() and len(p_tag.contents) == 1:
-                file_text = file_text + " " + p_tag.text
-        statement['file_text'] = file_text
-        if int(file['year']) > 2005:
-            date_search = soup.find("p",class_='article__time')
-            if date_search:
-                date = date_search.text
-        else:
-            date_search = soup.find("i")
-            if date_search:
-                date = date_search.text.split(": ")[1]
-        if date:
-            formatting = "%B %d, %Y"
-            date_object = datetime.datetime.strptime(date, formatting).date()
-            statement['release_date'] = date_object
-        else:
-            statement['release_date'] = None
-        statements.append(statement)
-    write_statement_csv(statements)
+        if grouping == "Statement":
+            tika.initVM()
+            document =  download_html(file)
+        elif grouping == "Bluebook":
+            document = download_pdf(file)
+        documents.append(document)
+    write_statement_csv(documents,grouping)
 
-def write_statement_csv(statements):
-    with open('../output/statement_data.csv', 'w') as csvfile:
-        fieldnames = ['start_date', 'end_date', 'release_date','file_text']
+def download_pdf(file):
+    document = {
+        'start_date': file['start_date'],
+        'end_date': file['end_date']
+    }
+    r = requests.get(file['link'])
+    open("../output/Bluebook/"+file['start_date'], 'wb').write(r.content)
+    parsed = parser.from_file("../output/Bluebook/"+file['start_date'])
+    document['file_text'] = parsed['content']
+    return document
+
+def download_html(file):
+    statement = {
+        'start_date': file['start_date'],
+        'end_date': file['end_date']
+    }
+    file_text = ""
+    r = requests.get(file['link']).content
+    soup = BeautifulSoup(r,'lxml')
+    p_tags = soup.find_all("p",attrs={'class':None})
+    for p_tag in p_tags:
+        if p_tag.text and p_tag.text.strip() and len(p_tag.contents) == 1:
+            file_text = file_text + " " + p_tag.text
+    statement['file_text'] = file_text
+    if int(file['year']) > 2005:
+        date_search = soup.find("p",class_='article__time')
+        if date_search:
+            date = date_search.text
+    else:
+        date_search = soup.find("i")
+        if date_search:
+            date = date_search.text.split(": ")[1]
+    if date:
+        formatting = "%B %d, %Y"
+        date_object = datetime.datetime.strptime(date, formatting).date()
+        statement['release_date'] = date_object
+    else:
+        statement['release_date'] = None
+    return statement
+
+def write_statement_csv(statements,grouping):
+    with open('../output/'+grouping+'.csv', 'w') as csvfile:
+        fieldnames = list(statements[0].keys())
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for statement in statements:
