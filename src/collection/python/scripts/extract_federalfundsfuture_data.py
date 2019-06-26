ad07529c5db39c22cd6d4ce4562d6ae548fc406b
OGiesecke <o.giesecke@gmail.com>
2019-06-26 10:33:19 -0400

    diverse changes
    Former-commit-id: 96a81cc1429160be35c4b1d2fab896d3b3287fc7

--- src/collection/python/scripts/extract_federalfundsfuture_data.py
+++ src/collection/python/scripts/extract_federalfundsfuture_data.py
@@ -11,10 +11,74 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+### Read the effective federal funds future data
+
+ffr_raw=pd.read_excel("../data/NYF_effective_federal_funds_data.xls",skiprows=4)
+ffr=ffr_raw[['EFFR\n(PERCENT)','DATE']]
+ffr.rename(columns={'EFFR\n(PERCENT)':'effr','DATE':'date'},inplace=True )    
+ffr=ffr[ffr.index<4648]
+
+def tranform_date(date):
+    return re.search('(\d{4}\-\d{2}\-\d{2})([\[r\]]*)',date,re.IGNORECASE).group(1)
+
+ffr.loc[:,'date']=ffr.loc[:,'date'].apply(tranform_date)
+ffr['date'] = pd.to_datetime(ffr['date'])
 
 ### Open the csv
 
 data=pd.read_excel("../data/FFF_1m3m_extract.xlsx")
+data['date'] = pd.to_datetime(data['date'])
+
+data=data.merge(ffr,how='left',on='date')
+
+
+######
+
+data['diff']= data['date'].diff(periods=-1)
+data['diff']=-data['diff']/pd.Timedelta(1, unit='d')
+
+data['month']=data['date'].apply(lambda x: x.month)
+data['year']=data['date'].apply(lambda x: x.year)
+
+
+def get_cont_ffr_exp(date,data):
+    print(date)
+    month=date.month
+    year=date.year
+    day=date.year
+    
+    # effective future series
+    newdata=data[(data['month']==month) & (data['year']==year) & (data['date']<=date)][['date','diff','effr']]
+    newdata['effr']=newdata['effr'].astype(float)
+    newdata['diff']=newdata['diff'].astype(int)
+    newdata['w_effr']=newdata['diff']*newdata['effr']
+    
+    FFF_0=data[data['date']==date]['FFF_0'].item()
+    print(FFF_0)
+    
+    if newdata[newdata['effr'].notna()]['diff'].sum()==0:
+        ffr_future=np.nan
+    else:  
+        ffr_past=newdata['w_effr'].sum(skipna=True)/newdata[newdata['effr'].notna()]['diff'].sum()
+    
+        acc_factor=30/(30-day)
+    
+        ffr_future=acc_factor*(1-FFF_0/100)+(1-acc_factor)*ffr_past/100
+    
+    print(ffr_future)
+    return ffr_future
+
+data.loc[:,"ffr_future"]=np.nan
+for idx,row in data.iterrows():
+    date=row['date']
+    ffr_future=get_cont_ffr_exp(date,data)
+    data.loc[idx,"ffr_future"]=ffr_future
+
+
+
+
+
+#######
 
 columns=data.columns
 rates=pd.DataFrame(data['date'])
