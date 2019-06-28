884f0de8550af3dcd5db0fe93ad17814fa49844c
Anand <anandc823@gmail.com>
2019-06-28 10:13:16 -0400

    added error handling, and swithched from list of dicts to dataframe
    Former-commit-id: 695ae884f3f8a5e29cc3e374ed671791f3215b6f

--- src/collection/python/scripts/download_newsarticles.py
+++ src/collection/python/scripts/download_newsarticles.py
@@ -62,109 +62,115 @@ def main():
 
     counter = len(end_dates)
     print("Found {} Meeting Dates".format(counter))
-    all_articles = []
+    all_articles = pd.DataFrame
     for end_date in end_dates:
-        print("Currently Working On Meeting With End Date:{}".format(end_date))
-        print("{} meetings left".format(counter))
-
-        article_date = end_date+timedelta(+1)
-        day=article_date.day
-        month=article_date.month
-        year=article_date.year
-
-        day_str=str(day).zfill(2)
-        month_str=str(month).zfill(2)
-        year_str=str(year).zfill(4)
-
-
-        ### Navigate and query the search page
-        query = '(Federal Open Market Committee or FOMC ) and (rst=nytf or rst=j or rst=ftfta)'
         try:
-            browser.find_element_by_xpath("//textarea[@name='ftx']").send_keys(len(query) * Keys.BACKSPACE)
-            browser.find_element_by_xpath("//textarea[@name='ftx']").send_keys(query)
+            print("Currently Working On Meeting With End Date:{}".format(end_date))
+            print("{} meetings left".format(counter))
 
-        except NoSuchElementException:
-            browser.find_element_by_xpath("//textarea[@class='ace_text-input']").send_keys(len(query) * Keys.BACKSPACE)
-            browser.find_element_by_xpath("//textarea[@class='ace_text-input']").send_keys(query)
-        browser.find_element_by_xpath('//*[@id="dr"]/option[contains(text(), "Enter date range...")]').click()
-        browser.find_element_by_id('frd').clear()
-        browser.find_element_by_id('frd').send_keys(day_str)
-        browser.find_element_by_id('frm').clear()
-        browser.find_element_by_id('frm').send_keys(month_str)
-        browser.find_element_by_id('fry').clear()
-        browser.find_element_by_id('fry').send_keys(year_str)
-        browser.find_element_by_id('tod').clear()
-        browser.find_element_by_id('tod').send_keys(day_str)
-        browser.find_element_by_id('tom').clear()
-        browser.find_element_by_id('tom').send_keys(month_str)
-        browser.find_element_by_id('toy').clear()
-        browser.find_element_by_id('toy').send_keys(year_str)
+            article_date = end_date+timedelta(+1)
+            day=article_date.day
+            month=article_date.month
+            year=article_date.year
 
-        wait_time = random.randint(15,25)
-        print("Waiting for {} Seconds...".format(wait_time))
-        time.sleep(wait_time)
+            day_str=str(day).zfill(2)
+            month_str=str(month).zfill(2)
+            year_str=str(year).zfill(4)
 
-        browser.find_element_by_xpath('//*[@id="btnSearchBottom"]').click()
 
-        ### Extract all the relevant links
-        article_results = []
-        article_containers = browser.find_elements_by_class_name('headline')
-        print("Found {} Articles".format(len(article_containers)))
-        for article_container in article_containers:
-            article_result = {}
-            article_result['link'] = article_container.find_element_by_class_name("enHeadline")
-            article_result['source'] = article_container.find_element_by_class_name("leadFields").\
-                find_element_by_tag_name("a").text
-            article_result['date'] = end_date.date()
-            article_result['headline'] = article_result['link'].text
-            article_results.append(article_result)
+            ### Navigate and query the search page
+            query = '(Federal Open Market Committee or FOMC ) and (rst=sfft)'
+            try:
+                browser.find_element_by_xpath("//textarea[@name='ftx']").send_keys(len(query) * Keys.BACKSPACE)
+                browser.find_element_by_xpath("//textarea[@name='ftx']").send_keys(query)
 
-        # parses article
-        for article in article_results:
-            article['link'].click()
-            wait_time = random.randint(15, 25)
-            print("Waiting for {} Seconds...".format(wait_time))
-            time.sleep(wait_time)
-            soup=BeautifulSoup(browser.page_source, 'html.parser')
-            #print([p for p in soup.find_all("p")])
-            article_text = soup.find_all('p',attrs={'class': 'articleParagraph enarticleParagraph'})
-            #print("Article Text looks like:{}".format(article_text))
-            cleaned_lines = []
-            for p_tag in article_text:
-                cleaned_lines.append(' '.join(filter(None, p_tag.getText().split('\n'))))
-            content=' '.join(cleaned_lines)
+            except NoSuchElementException:
+                browser.find_element_by_xpath("//textarea[@class='ace_text-input']").send_keys(len(query) * Keys.BACKSPACE)
+                browser.find_element_by_xpath("//textarea[@class='ace_text-input']").send_keys(query)
+            browser.find_element_by_xpath('//*[@id="dr"]/option[contains(text(), "Enter date range...")]').click()
+            browser.find_element_by_id('frd').clear()
+            browser.find_element_by_id('frd').send_keys(day_str)
+            browser.find_element_by_id('frm').clear()
+            browser.find_element_by_id('frm').send_keys(month_str)
+            browser.find_element_by_id('fry').clear()
+            browser.find_element_by_id('fry').send_keys(year_str)
+            browser.find_element_by_id('tod').clear()
+            browser.find_element_by_id('tod').send_keys(day_str)
+            browser.find_element_by_id('tom').clear()
+            browser.find_element_by_id('tom').send_keys(month_str)
+            browser.find_element_by_id('toy').clear()
+            browser.find_element_by_id('toy').send_keys(year_str)
 
-            #Final Article Data for Export
-            article_entry ={'meeting_date':end_date,
-                     'article_date':article_date, 'source': str(article['source']),
-                     'headline':article['headline'],'content':[content]}
-
-            print("FINAL ARTICLE LOOKS LIKE:{}".format(article_entry))
-
-            all_articles.append(article_entry)
-
-            print("All Articles now contains {}".format(len(all_articles)))
             wait_time = random.randint(15,25)
             print("Waiting for {} Seconds...".format(wait_time))
             time.sleep(wait_time)
-            browser.execute_script("window.history.go(-1)")
-        wait_time = random.randint(15, 25)
-        print("Waiting for {} Seconds...".format(wait_time))
-        time.sleep(wait_time)
-        browser.execute_script("window.history.go(-1)")
-        wait_time = random.randint(15, 25)
-        print("Waiting for {} Seconds...".format(wait_time))
-        time.sleep(wait_time)
-        counter-=1
-    browser.quit()
-    write_derived_csv(all_articles)
 
-def write_derived_csv(documents):
-    with open('../output/news_articles_test.csv', 'w') as csvfile:
-        fieldnames = documents[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for document in documents:
-            writer.writerow(document)
+            browser.find_element_by_xpath('//*[@id="btnSearchBottom"]').click()
+            try:
+                ### Extract all the relevant links
+                article_search_results = []
+                article_containers = browser.find_elements_by_xpath('//div[@class="headlines"]/table/tbody/tr[@class="headline"]')
+                print("Found {} Articles".format(len(article_containers)))
+                for article_container in article_containers:
+                    try:
+                        article_result = {}
+                        article_result['link'] = article_container.find_element_by_class_name("enHeadline")
+                        article_result['source'] = article_container.find_element_by_class_name("leadFields").\
+                            find_element_by_tag_name("a").text
+                        article_result['date'] = end_date.date()
+                        article_result['headline'] = article_result['link'].text
+                        article_search_results.append(article_result)
+                    except:
+                        print("ERROR GETTING ARTICLE RESULT INFO FOR DATE {}".format(end_date))
+
+                # parses article
+                for article in article_search_results:
+                    article['link'].click()
+                    try:
+                        wait_time = random.randint(15, 25)
+                        print("Waiting for {} Seconds...".format(wait_time))
+                        time.sleep(wait_time)
+                        soup=BeautifulSoup(browser.page_source, 'html.parser')
+                        #print([p for p in soup.find_all("p")])
+                        article_text = soup.find_all('p',attrs={'class': 'articleParagraph enarticleParagraph'})
+                        #print("Article Text looks like:{}".format(article_text))
+                        cleaned_lines = []
+                        for p_tag in article_text:
+                            cleaned_lines.append(' '.join(filter(None, p_tag.getText().split('\n'))))
+                        content=' '.join(cleaned_lines)
+
+                        #Final Article Data for Export
+                        article_entry ={'meeting_date':end_date,
+                                 'article_date':article_date, 'source': str(article['source']),
+                                 'headline':article['headline'],'content':[content]}
+
+                        print("FINAL ARTICLE LOOKS LIKE:{}".format(article_entry))
+
+                        all_articles.append(article_entry,ignore_index=True)
+
+                        print("All Articles now contains {}".format(len(all_articles)))
+                        wait_time = random.randint(15,25)
+                        print("Waiting for {} Seconds...".format(wait_time))
+                        time.sleep(wait_time)
+                        browser.execute_script("window.history.go(-1)")
+                    except:
+                        print("ERROR looking in article {}".format(article))
+                        browser.execute_script("window.history.go(-1)")
+                wait_time = random.randint(15, 25)
+                print("Waiting for {} Seconds...".format(wait_time))
+                time.sleep(wait_time)
+                browser.execute_script("window.history.go(-1)")
+            except:
+                print("error for article with date {}".format(end_date))
+                browser.execute_script("window.history.go(-1)")
+            wait_time = random.randint(15, 25)
+            print("Waiting for {} Seconds...".format(wait_time))
+            time.sleep(wait_time)
+            counter-=1
+        except:
+            print("ERROR ON SEARCH PAGE FOR DATE {}".format(end_date))
+    browser.quit()
+    all_articles.to_csv("../output/ft_articles.csv")
+
 
 main()
