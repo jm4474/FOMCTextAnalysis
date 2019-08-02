90bf597260f48286109cc7ff5a2ae053b1e3a6c2
Anand <anandc823@gmail.com>
2019-08-02 17:00:21 -0400

    removed end date from monthly data and removed monthly measures from daily, fixed january problem
    Former-commit-id: 7eeb798dc2a8fffb49996a09f2333845039dcf9e

--- src/collection/python/scripts/merge_string_theory_sources.py
+++ src/collection/python/scripts/merge_string_theory_sources.py
@@ -9,6 +9,13 @@ def main():
     pcepi = pd.read_csv("../data/string_theory/PCEPI.csv")
     unrate = pd.read_csv("../data/string_theory/UNRATE.csv")
 
+    monthly_dataframes = [indpro,pcepi,unrate]
+    for dataframe in monthly_dataframes:
+        dataframe['DATE'] = pd.to_datetime(dataframe['DATE'])
+    monthly_merged = reduce(lambda left,right: \
+                           pd.merge(left,right,on=['DATE'],how='outer'),monthly_dataframes)
+    monthly_merged.to_csv("../output/string_theory_indicators_monthly.csv")
+
     treasury_yields = pd.read_csv("../data/string_theory/FRB_H15.csv")
     treasury_yields['DATE'] = pd.to_datetime(treasury_yields['Unique Identifier: '],errors="coerce")
     treasury_yields = treasury_yields.dropna(subset=['DATE'])
@@ -24,17 +31,15 @@ def main():
     futures_contract = futures_contract[['DATE','FF1_COMDTY','FF2_COMDTY']]
     futures_contract = futures_contract.dropna(subset=["DATE"])
 
-    dataframes = [dfedtar,dfedtarl,dfedtaru,dff,indpro,pcepi,unrate,treasury_yields,futures_contract]
+    dataframes = [dfedtar,dfedtarl,dfedtaru,dff,treasury_yields,futures_contract]
     for dataframe in dataframes:
         dataframe['DATE'] = pd.to_datetime(dataframe['DATE'])
     df_merged = reduce(lambda left,right: \
                            pd.merge(left,right,on=['DATE'],how='outer'),dataframes)
 
     df_merged['DATE'] = pd.to_datetime(df_merged['DATE'])
-    monthly = df_merged[df_merged['DATE'].dt.day==1]
 
     df_merged.to_csv("../output/string_theory_indicators_daily.csv")
-    monthly.to_csv("../output/string_theory_indicators_monthly.csv")
 
 if __name__ == "__main__":
     main()
