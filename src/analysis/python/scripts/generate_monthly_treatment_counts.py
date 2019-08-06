47d2da5fb94dda73ea6046fbf2055f01b7cf6afb
Anand <anandc823@gmail.com>
2019-08-06 11:20:58 -0400

    added dec,unch,increase variables to treatment counts
    Former-commit-id: fffb1a17f6baf7164b7a1e4ab062e108213e8b81

--- src/analysis/python/scripts/generate_monthly_treatment_counts.py
+++ src/analysis/python/scripts/generate_monthly_treatment_counts.py
@@ -14,15 +14,25 @@ def main():
     column_names = ['month','year']
     sizes = [-0.75,-0.5,-.25,0,.25,.5,.75]
     for size in sizes:
-        treatments[str(size)+'a'] = pd.to_numeric(treatments['tsa'],errors='coerce')==size
-        treatments[str(size)+'b'] = pd.to_numeric(treatments['tsb'],errors='coerce')==size
-        treatments[str(size)+'c'] = pd.to_numeric(treatments['tsc'],errors='coerce')==size
+        for alt in ['a','b','c']:
+            treatments[str(size)+alt] = pd.to_numeric(treatments['ts'+alt],errors='coerce')==size
+            treatments['d_dec_'+alt] = pd.to_numeric(treatments['ts'+alt],errors='coerce')<0
+            treatments['d_unc_' + alt] = pd.to_numeric(treatments['ts' + alt],errors='coerce') == 0
+            treatments['d_inc_' + alt] = pd.to_numeric(treatments['ts' + alt],errors='coerce') > 0
+
         size_name = 'd_{sign}{val}'.format(
             sign="m" if size < 0 else "", val=str(abs(int(size * 100))))
         print(size_name)
         treatments[size_name] = treatments[[str(size)+'a',str(size)+'b',
                                             str(size)+'c']].sum(axis=1)
+        treatments['d_dec'] = treatments[['d_dec_a','d_dec_b','d_dec_c']].sum(axis=1)
+        treatments['d_unc'] = treatments[['d_unc_a','d_unc_b','d_unc_c']].sum(axis=1)
+        treatments['d_inc'] = treatments[['d_inc_a','d_inc_b','d_inc_c']].sum(axis=1)
+
         column_names.append(size_name)
+    column_names.append('d_dec')
+    column_names.append('d_unc')
+    column_names.append('d_inc')
     treatments['month'] = pd.to_datetime(treatments.date).dt.month
     treatments['year'] = pd.to_datetime(treatments.date).dt.year
     treatments = treatments[column_names]
@@ -32,6 +42,6 @@ def main():
             month_df = month_df.append({'month':month,'year':year},ignore_index=True)
     treatments = treatments.merge(month_df,on=['month','year'],how="outer").fillna(0)
     print(treatments)
-    treatments.to_csv("monthly_treatment_counts.csv")
+    treatments.to_csv("../output/monthly_treatment_counts.csv")
 if __name__ == "__main__":
     main()
