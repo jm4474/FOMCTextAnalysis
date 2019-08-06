33d337c787760cfc6a4efcac43232c16ebef39a7
Anand <anandc823@gmail.com>
2019-08-06 12:35:21 -0400

    removed 2009 from set
    Former-commit-id: 30c2e951943ac21787deea3c29fa028217126dc3

--- src/analysis/python/scripts/generate_monthly_treatment_counts.py
+++ src/analysis/python/scripts/generate_monthly_treatment_counts.py
@@ -26,7 +26,6 @@ def main():
     treatments['d_inc'] = ((treatments['tsa'] > 0) \
                            | (treatments['tsb'] > 0) | \
                            (treatments['tsc'] > 0)).astype(int)
-    print(treatments)
     column_names = ['d_dec','d_unc','d_inc']
     sizes = [-0.75,-0.5,-.25,0,.25,.5,.75]
     for size in sizes:
@@ -41,15 +40,16 @@ def main():
 
     treatments['month'] = pd.to_datetime(treatments.date).dt.month
     treatments['year'] = pd.to_datetime(treatments.date).dt.year
+
     column_names.append('month')
     column_names.append('year')
     treatments = treatments[column_names]
     month_df = pd.DataFrame()
-    for year in range(1988,2010):
+    for year in range(1988,2009):
         for month in range(1,13):
             month_df = month_df.append({'month':month,'year':year},ignore_index=True)
     treatments = treatments.merge(month_df,on=['month','year'],how="outer").fillna(0)
-    print(treatments.sort_values(by=['year','month']))
+
     treatments.to_csv("../output/monthly_treatment_counts.csv")
 if __name__ == "__main__":
     main()
