4ebbcd17e6b21a90db363d08455e552d56e7c4a0
Anand <anandc823@gmail.com>
2019-08-08 12:25:42 -0400

    added news expectation scores for other topics

--- src/analysis/python/scripts/extract_text_expectations.py
+++ src/analysis/python/scripts/extract_text_expectations.py
@@ -1,33 +1,60 @@
 import os
 import pandas as pd
+import numpy as np
 import re
+import matplotlib.pyplot as plt
 #'expectation','inflation','unemployment','rates','market expectations',
 #                  'federal funds rate',
 
 def main():
     df = load_news()
     for i in df.index:
-        raw_text = df.loc[i,'raw_text']
+        raw_text = str(df.loc[i,'raw_text']).replace("\\","").lower()
+        #print(raw_text)
         sentences = raw_text.split(". ")
-        exp_sents = [sentence+'.' for sentence in sentences]
-        topics = ['surprise']
-        if exp_sents:
-            df.loc[i, 'sentences'] = "\n".join(exp_sents)
-            df.loc[i, 'sentence_counts'] = str(len(exp_sents))
-            for topic in topics:
-                if topic in "".join(exp_sents):
-                    topic_sents = []
-                    for sentence in exp_sents:
-                        if topic in sentence:
-                            topic_sents.append(sentence)
-                    df.loc[i, "d_" + topic.replace(" ", "_")] = 1
-                    df.loc[i, 'sent_' + topic.replace(" ", "_")] = "\n".join(topic_sents)
+        topics = ['surpris','uncertain','unexpected','not expect']
+        df.loc[i, 'sentences'] = ". ".join(sentences)
+        df.loc[i, 'sentence_counts'] = len(sentences)
+        for topic in topics:
+            if topic in "".join(sentences):
+                topic_sents = []
+                for sentence in sentences:
+                    if topic in sentence and not is_negated(sentence.split(),topic):
+                        topic_sents.append(sentence)
+                df.loc[i, "d_" + topic.replace(" ", "_")] = 1
+                df.loc[i, 'sent_' + topic.replace(" ", "_")] = ". ".join(topic_sents)
     df['n_article'] = 1
+    export_news(df,topics)
+
+def export_bluebook(df,topics):
+
+    df = df.sort_values(by="date")
+    df.to_csv("../output/bluebook_text_expectations.csv")
+
+def export_news(df,topics):
     grp = df.groupby('date').sum()
+    grp['score'] = 0
     grp['month'] = pd.to_datetime(grp.index).month
     grp['year'] = pd.to_datetime(grp.index).year
-    grp['std_surprise'] = grp['d_surprise']/grp['n_article']
+    for topic in topics:
+        score_col = 'std_'+topic.replace(" ","_")
+        grp[score_col] = np.divide(grp["d_"+topic.replace(" ","_")],grp['n_article'])
+        grp['score'] += grp[score_col]
+    grp['score'] = grp['score']/len(topics)
     grp.to_csv("../output/news_text_expectations.csv")
+    df.to_csv("../output/news_text_expectations_sentences.csv")
+def is_negated(words,word):
+    negation = ['no','not','lack','wasn','didn']
+    word_indexes = []
+    for index,cur_word in enumerate(words):
+        if word in cur_word:
+            word_indexes.append(index)
+    for word_index in word_indexes:
+        for window in range(1,4):
+            if word_index-window>0:
+                if words[word_index-window] in negation:
+                    return True
+    return False
 def load_bluebook():
     dir = "../../../collection/python/output/bluebook_raw_text"
     df = pd.DataFrame()
