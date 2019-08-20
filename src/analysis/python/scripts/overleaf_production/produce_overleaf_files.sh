5abadfa8e6c406a5747678e0506c50db8f42b726
OGiesecke <o.giesecke@gmail.com>
2019-08-20 09:12:31 -0400

    Update Overleaf figures
    Former-commit-id: 154b8232f5a27f1cfdd8434e3e14b427e5f7d0ba

--- src/analysis/python/scripts/overleaf_production/produce_overleaf_files.sh
+++ src/analysis/python/scripts/overleaf_production/produce_overleaf_files.sh
@@ -4,10 +4,11 @@ python analyze_statements.py
 python decade_file_counts.py
 python generate_rateless_alt_action.py
 python obtain_sumstats_bb_options.py
-python produce_bb_size_sumstats.py
 python produce_federal_funds_graphs.py
 python produce_grouped_percent_treatment.py
 python produce_news_stats.py
 python produce_rate_change_source.py
 python produce_sumstats_menu.py
 python produce_target_and_alternative_data.py
+python produce_bb_size_sumstats.py
+
