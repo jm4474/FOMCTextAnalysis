95d14707cc6f085ac2a84c8920b3281903d10a86
Anand <anandc823@gmail.com>
2019-12-08 18:42:20 -0500

    added year labels

--- src/analysis/python/scripts/produce_target_and_alternative_graph.py
+++ src/analysis/python/scripts/produce_target_and_alternative_graph.py
@@ -20,29 +20,29 @@ def plot_target(dataffr):
     plt.rc('text', usetex=True)
     fig = plt.figure(figsize=(10, 3))
     ax = fig.add_subplot(1, 1, 1)
+    dataffr.sort_values(by="date",inplace=True)
+    dataffr['date'] = pd.to_datetime(dataffr['date'])
     ax.plot(dataffr['date'], dataffr['alt_a_rate'], 'bo',markersize=1)
     ax.plot(dataffr['date'], dataffr['alt_b_rate'], 'bo',markersize=1)
     ax.plot(dataffr['date'], dataffr['alt_c_rate'], 'bo',markersize=1)
     ax.plot(dataffr['date'], dataffr['alt_d_rate'], 'bo',markersize=1)
     ax.plot(dataffr['date'], dataffr['alt_e_rate'], 'bo',markersize=1,label="Policy Menu")
-
+    
     ax.plot(dataffr['date'], dataffr['target_after'], 'yo', markersize=3,fillstyle='none',label="Target After Meeting")
     ax.legend(loc='upper right')
-    years = mdates.YearLocator()  # every year
-    months = mdates.MonthLocator()  # every month
+    years = mdates.YearLocator(2)  # every 2 years
+    #months = mdates.MonthLocator()  # every month
     yearsFmt = mdates.DateFormatter('%Y')
 
     ax.xaxis.set_major_locator(years)
     ax.xaxis.set_major_formatter(yearsFmt)
-    ax.xaxis.set_minor_locator(months)
-
-
-
+    
     #turning_points = ['1989-06-01', '1993-06-01', '1995-04-01', '2000-11-01', '2004-01-01', '2007-02-01']
     #for datestring in turning_points:
     #    ax.axvline(x=pd.to_datetime(datestring), color='gray', linestyle='--')
 
-    #plt.legend(['Federal Funds Alternatives and Actions'], frameon=False)
+    #plt.legend(['Federal Funds Alternatives and Actions'], frameon=False)]
+    #plt.xticks(dataffr[])
     plt.savefig('../output/overleaf_files/fig_fed_target_and_alt.png', dpi=300, bbox_inches='tight')
     dataffr['decision'] = dataffr['target_after']-dataffr['target_before']
     dataffr.loc[168,'decision'] = -.5
